d2790fecf9babaa91cb14a7a44cb231cea3afca4
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-12-12 15:06:42 +0100

    Adderet ekstra Controller.

--- WebApiStudentDataClient/Project_Scripts/Settings.js
+++ WebApiStudentDataClient/Project_Scripts/Settings.js
@@ -1,6 +1,6 @@
-﻿const WEB_API_URL_StudentData = "http://localhost:62090/api/";
+﻿//const WEB_API_URL_StudentData = "http://localhost:62090/api/";
 
-//const WEB_API_URL_StudentData = "http://webapistudentdata.buchwaldshave34.dk/api/";
+const WEB_API_URL_StudentData = "http://webapistudentdata.buchwaldshave34.dk/api/";
 
 const WEB_API_URL_StudentData_Course = WEB_API_URL_StudentData + "Course";
 const WEB_API_URL_StudentData_Eductaion = WEB_API_URL_StudentData + "Education";
@@ -8,6 +8,7 @@ const WEP_API_URL_USERINFO = WEB_API_URL_StudentData + "UserInfo";
 const WEB_API_URL_RETURN_CODES_AND_RETURN_STRINGS = WEB_API_URL_StudentData + "ReturnCodesAndStrings";
 
 const WEB_API_URL_StudentData_EducationLine = WEB_API_URL_StudentData + "EducationLine";
+const WEB_API_URL_StudentData_ContactForm = WEB_API_URL_StudentData + "ContactForm";
 
 const UserName = "Lars-Lærer";
 const Password = "smal";
